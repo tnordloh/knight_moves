e597368ee44a81eea0853242184fc25ed11634a6
Timothy Nordloh <tnordloh@gmail.com>
2014-09-23 09:41:27 -0600

    fix end arguments

--- bin/findpath.rb
+++ bin/findpath.rb
@@ -2,7 +2,6 @@
 require_relative "../lib/chess.rb"
 p=Chess::Path.new()
 if ARGV.size != 2
-  puts "expecting 2 arguments"
-  exit 1
+  abort "expecting 2 arguments"
 end
 puts p.find_path(ARGV[0],ARGV[1])
